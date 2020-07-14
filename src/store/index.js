be512af72ba5856d48e3db393cd96adeef1969ec
Carcair <delic.emir90@gmail.com>
2020-07-14 13:59:04 +0200

    Imported seperate store modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Messages from './modules/Messages';
+import Schedules from './modules/Schedules';
+import Triggers from './modules/Triggers';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    Messages,
+    Schedules,
+    Triggers,
+  },
 });
